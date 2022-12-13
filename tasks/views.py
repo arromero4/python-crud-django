74ab91e81b38568fe7a0901217caae057b70ab08
arromero4 <arromero0408@gmail.com>
2022-12-13 00:19:02 -0600

    CRUD with Django

--- tasks/views.py
+++ tasks/views.py
@@ -1,27 +1,70 @@
-from django.shortcuts import render
-from django.contrib.auth.forms import UserCreationForm
+from django.shortcuts import render, redirect
+from django.contrib.auth.forms import UserCreationForm, AuthenticationForm
 from django.contrib.auth.models import User
-from django.http import HttpResponse
-
+from django.db import IntegrityError
+from django.contrib.auth import login, logout, authenticate
+from .forms import TaskForm
 # Create your views here.
+
+
 def home(request):
     return render(request, 'home.html')
+
 
 def signup(request):
 
     if request.method == 'GET':
         return render(request, 'signup.html', {
-        'form': UserCreationForm,
-    })
+            'form': UserCreationForm,
+        })
     else:
         if request.POST['password1'] == request.POST['password2']:
             try:
-                #register user
-                user = User.objects.create_user(username = request.POST['username'], password = request.POST['password1'])
+                # register user
+                user = User.objects.create_user(
+                    username=request.POST['username'], password=request.POST['password1'])
                 user.save()
-                return HttpResponse('User created successfully!')
-            except:
-                return HttpResponse('Username already exists.')
-        return HttpResponse('Password do not match.')
-       
-    
+                login(request, user)
+                return redirect('tasks')
+            except IntegrityError:
+                return render(request, 'signup.html', {
+                    'form': UserCreationForm,
+                    'error': 'Username already exists'
+                })
+        return render(request, 'signup.html', {
+            'form': UserCreationForm,
+            'error': 'Password do not match.'
+        })
+
+def tasks(request):
+    return render(request,'tasks.html')
+
+def signout(request):
+    logout(request)
+    return redirect('home')
+
+def signin(request):
+    if request.method == 'GET':
+        return render(request,'signin.html',{
+            'form': AuthenticationForm
+        })
+    else:
+        user = authenticate(request, username=request.POST['username'], password=request.POST['password'])
+        if user is None:
+            return render(request,'signin.html',{
+            'form': AuthenticationForm,
+            'error': 'Username or password is incorrect.'
+            })
+        else:
+            login(request, user)
+            return redirect('tasks')
+
+def create_task(request):
+    if request.method == 'GET':
+        return render(request, 'create_task.html',{
+        'form': TaskForm
+        })
+    else:
+        return render(request, 'create_task.html',{
+        'form': TaskForm
+        })       

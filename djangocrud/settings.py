9f0250c406c5a1912c8a789891384ebab58ce068
arromero4 <arromero0408@gmail.com>
2023-01-08 17:51:16 -0600

    CRUD with Django

--- djangocrud/settings.py
+++ djangocrud/settings.py
@@ -86,6 +86,7 @@ DATABASES = {
         # 'ENGINE': 'django.db.backends.sqlite3',
         # 'NAME': BASE_DIR / 'db.sqlite3',
         'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'postgresql',
         'default': dj_database_url.config(
             # Feel free to alter this value to suit your needs.        
             default='postgresql://postgres:postgres@localhost:5432/djangocrud', conn_max_age=600)
